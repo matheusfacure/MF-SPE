834e290fd18d2cbb410e0ee4fc2407235a91afc7
Matheus Facure <matheusfacure01@gmail.com>
2016-07-13 11:17:58 -0300

    Tenta reestruturar scrape de top 5

--- FOCUS.py
+++ FOCUS.py
@@ -4,6 +4,7 @@ import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
+import numpy as np
 
 #Funções
 
@@ -24,7 +25,7 @@ def getValues(page):
 	endTable = page.find('ultima linha em branco', startTable)
 	return re.findall('\d\,\d\d', page[startTable:endTable])
 
-
+#Pega os valores dos IPs e dos cálculos especificados
 def geralScrapeIPs(IPs, calculos, anos):
 	
 	#Seleções Padrão
@@ -63,75 +64,54 @@ def geralScrapeIPs(IPs, calculos, anos):
 			time.sleep(0.5)
 	return inDic
 
-
-
-
-
-
-#Seleciona na página de pequisa
-def TOP5Select(ranking, periodicidade, anos):
-
-	#seleciona indicadores Top 5
+#Pega os valores dos indicadores TOP5 e dos calculos especificados
+def TOP5ScrapeAnual(indicadores, calculos, anos):
+	
+	#Seleções Padrão
 	select('#indicador', 'Indicadores do Top 5')
-	
-	#seleciona ranking de médio prazo
-	select('#tipoRanking', ranking)
-	
-	#sleciona periodicidade anual
 	select('#periodicidade', 'Anual')
-	
-	#seleciona período que a projeção foi feita
 	dataFinal = driver.find_element_by_css_selector('#tfDataInicial1')
 	dataFinal.send_keys(time.strftime("%d/%m/%Y"))
 	dataInicial = driver.find_element_by_css_selector('#tfDataFinal2')
 	dataInicial.send_keys(time.strftime("%d/%m/%Y"))
 	
-	#Seleciona começo e fim da série (ano atual e próximo)
-	select('#form4 > div.centralizado > table > tbody:nth-child(8) >' \
-		'tr > td:nth-child(2) > select', anos[0])
-	select('#form4 > div.centralizado > table > tbody:nth-child(8) >' \
-		'tr > td:nth-child(4) > select', anos[-1])
 
-#Pega os valores dos indicadores e dos calculos especificados
-def TOP5Scrape(anos, ranking, periodicidade, indicadores, calculos):
+	ranking = ['Médio Prazo Mensal', 'Longo Prazo']
 	
-	#Seleçiona primeiros dois anos e ranking Médio Prazo Mensal
-	TOP5Select(ranking, periodicidade, anos)
+	for rk in ranking:
+		select('#tipoRanking', rk)
 
-	top5Anual = {} #cria dicinário p/ armazenar indicies
-	
-	#Seleções personalizadas
-	for ind in indicadores:
-
-		if ind not in top5Anual:
-			print(ind)
-			top5Anual[ind] = {} #cria sub-dicionário p/ valores
-
-		for calc in calculos:
-
-			driver.find_element_by_css_selector(indicadores[ind][0]).click()
-			select('#calculo', calc) #seleciona metodo de cálculo
-			
-			if indicadores[ind][0] == '#opcoesd_3': #se o indicador for o câmbio
-				select('#tipoDeTaxa', 'Fim de ano') #seleciona taxa fim de ano
-			
-			driver.find_element_by_css_selector('#btnConsultar8').click() # ir
-
-			time.sleep(1) #previne bugs por internet lenta
-			
-			source = driver.page_source
-			print(getValues(source))
-
-			if calc not in top5Anual[ind]:
-				top5Anual[ind][calc] = getValues(source)
-			else:
-				top5Anual[ind][calc].append(getValues(source))
-
-			print(top5Anual) 
+		
+		select('#form4 > div.centralizado > table > tbody:nth-child(8) >' \
+			'tr > td:nth-child(2) > select', anos[0])
+		select('#form4 > div.centralizado > table > tbody:nth-child(8) >' \
+			'tr > td:nth-child(4) > select', anos[1])
 
 
-			driver.back()
-			time.sleep(1)
+		for ind in indicadores:
+			driver.find_element_by_css_selector(indicadores[ind]).click()
+			if(ind == "Câmbio"): #Se for a taxa de câmbio, seleciona fim de ano
+				select('#tipoDeTaxa', 'Fim de ano')
+
+			for calc in calculos:
+				select('#calculo', calc)
+				driver.find_element_by_css_selector('#btnConsultar8').click()
+				time.sleep(0.5) #previne bugs por internet lenta
+				source = driver.page_source
+
+				print(getValues(source))
+
+				driver.back()
+				time.sleep(0.5)
+
+
+
+
+
+
+
+
+
 
 
 
@@ -147,9 +127,9 @@ for ano in range(0,5):
 	a = atual + datetime.timedelta(days = 366*ano)
 	anos.append(a.strftime('%Y'))
 
+calculos = ['Mínimo', 'Mediana', 'Máximo', 'Média', 'Desvio padrão']
 
 #Geral
-calculos = ['Mínimo', 'Mediana', 'Máximo', 'Média', 'Desvio padrão']
 
 #IPs
 IPs = {'IPCA':'#grupoIndicePreco\:opcoes_5',
@@ -161,33 +141,24 @@ print(df)
 df.to_csv('teste.csv', sep=';')
 
 #output para teste
-dicionario = {'IPCA': {'Mediana': ['7,26', '5,40', '4,71', '4,50', '4,50'], 
-	'Anos': ['2016', '2017', '2018', '2019', '2020'],
-	'Média': ['7,27', '5,36', '4,85', '4,66', '4,60'],
-	'Desvio padrão': ['0,29', '0,47', '0,47', '0,39','0,42'],
-	'Mínimo': ['6,47', '4,50', '4,00', '3,80', '3,70'],
-	'Máximo': ['8,27', '7,48', '6,80', '6,00', '6,00']},
-	'IGP-DI': {'Mediana': ['9,20', '5,57', '5,05', '4,70', '4,55'],
-	'Anos': ['2016', '2017', '2018', '2019', '2020'],
-	'Média': ['9,08', '5,57', '5,21', '4,87', '4,79'],
-	'Desvio padrão': ['0,86', '0,57', '0,61', '0,49', '0,50'],
-	'Mínimo': ['6,10', '4,20', '4,00', '4,00', '4,00'],
-	'Máximo': ['0,73', '6,79', '6,80', '6,00', '6,00']}}
+#dicionarioteste = {'IPCA': {'Mediana': ['7,26', '5,40', '4,71', '4,50', '4,50'], 
+#	'Anos': ['2016', '2017', '2018', '2019', '2020'],
+#	'Média': ['7,27', '5,36', '4,85', '4,66', '4,60'],
+#	'Desvio padrão': ['0,29', '0,47', '0,47', '0,39','0,42'],
+#	'Mínimo': ['6,47', '4,50', '4,00', '3,80', '3,70'],
+#	'Máximo': ['8,27', '7,48', '6,80', '6,00', '6,00']},
+#	'IGP-DI': {'Mediana': ['9,20', '5,57', '5,05', '4,70', '4,55'],
+#	'Anos': ['2016', '2017', '2018', '2019', '2020'],
+#	'Média': ['9,08', '5,57', '5,21', '4,87', '4,79'],
+#	'Desvio padrão': ['0,86', '0,57', '0,61', '0,49', '0,50'],
+#	'Mínimo': ['6,10', '4,20', '4,00', '4,00', '4,00'],
+#	'Máximo': ['0,73', '6,79', '6,80', '6,00', '6,00']}}
 
 
 #TOP5
 
-#cria lista de cálculos
-#calculos = ['Mínimo', 'Mediana', 'Máximo', 'Média', 'Desvio padrão']
-
 #cria dicinário de indicadores
-#indicadores = {'IGP-DI':['#opcoesd_0', 'IGP-DI']}
-	#'IPCA':['#opcoesd_2', 'IPCA'],'Câmbio':['#opcoesd_3', 'Câmbio']}
-
-#print(anos)
-
-#Anual
-#TOP5Scrape(anos[0:2], 'Médio Prazo Mensal', 'Anual', indicadores, calculos)
-#TOP5Scrape(anos[2:], 'Longo Prazo', 'Anual', indicadores, calculos)
-
-
+#indicadores = {'IGP-DI':'#opcoesd_0', 'IPCA':'#opcoesd_2',
+#	'Câmbio':'#opcoesd_3'}
+#
+#TOP5ScrapeAnual(indicadores, calculos, anos)
